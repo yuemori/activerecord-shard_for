0d2ded7d617a8334bd9128c64134ba053cf591b1
yuemori <yuemori@aiming-inc.com>
2017-02-06 01:37:46 +0900

    Implement sharing connection for same shard

--- lib/activerecord/shard_for/shard_repository.rb
+++ lib/activerecord/shard_for/shard_repository.rb
@@ -9,6 +9,7 @@ module ActiveRecord
         @base_class = base_class
 
         @shards = cluster_config.connection_registry.each_with_object({}) do |(key, connection_name), hash|
+          generate_connection_publisher_unless_exist(connection_name)
           model = generate_model_for_shard(connection_name, key)
           base_class.const_set(:"#{generate_class_name(connection_name)}", model)
           hash[connection_name] = model
@@ -17,29 +18,50 @@ module ActiveRecord
 
       private
 
+      # Generate connection publisher to shard.
+      # More infomation, see `establish_connection to same shard` test in the model_spec.rb.
+      def generate_connection_publisher_unless_exist(connection_name)
+        class_name = generate_class_name(connection_name)
+
+        return if Object.const_defined?(:"#{class_name}")
+
+        model = Class.new(ActiveRecord::Base) do
+          module_eval <<-RUBY, __FILE__, __LINE__ + 1
+            def self.name
+              "#{class_name}"
+            end
+          RUBY
+        end
+        Object.const_set(:"#{class_name}", model)
+        model.establish_connection connection_name
+      end
+
       # @param [Symbol] connection_name
       # @param [Range] slot_range
       # @return [Class] A sub class of given AR model.
       #   A sub class has connection setting for specific shard.
-      def generate_model_for_shard(connection_name, key)
+      def generate_model_for_shard(connection_name, key) # rubocop:disable Metrics/MethodLength
         class_name = generate_class_name(connection_name)
 
-        model = Class.new(base_class) do
+        Class.new(base_class) do
           self.table_name = base_class.table_name
           class << self
             attr_reader :assigned_key
           end
           @assigned_key = key
+          # For Rails5.0 only
+          self.connection_specification_name = class_name if respond_to?(:connection_specification_name)
 
           module_eval <<-RUBY, __FILE__, __LINE__ + 1
             def self.name
               "#{base_class.name}::#{class_name}"
             end
+
+            def self.connection
+              ::#{class_name}.connection
+            end
           RUBY
         end
-
-        model.class_eval { establish_connection(connection_name) }
-        model
       end
     end
   end
